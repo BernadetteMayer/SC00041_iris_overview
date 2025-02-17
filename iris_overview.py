6433ac31aba33e1a6f30b231045e9d10ed52ec02
EmmaCAndersson <emma.c.j.s.andersson@gmail.com>
2025-02-17 14:54:19 +0100

    Plot iris data

---
+++ iris_overview.py
@@ -0,0 +1,16 @@
+# python read iris dataset and do stuff
+# import seaborn package 
+import seaborn as sns
+
+# load the iris dataset
+iris = sns.load_dataset("iris")
+
+# TODO: Import datetime and create timestamp for plot file
+
+g = sns.PairGrid(iris, hue="species")
+g.map_diag(sns.histplot)
+g.map_offdiag(sns.scatterplot)
+g.add_legend()
+
+# save plot
+g.figure.savefig("/home/xaemmo/teaching/reproducible_code/assignment_repos/output.png")
